2f546f8ade4c41950c592b4610d80b3a8e295239
Schossow <68172401+Schossow@users.noreply.github.com>
2020-07-31 15:22:29 -0500

    Actualizacion index

---
+++ js/anexoScript.js
@@ -0,0 +1,16 @@
+
+document.getElementById("btnAbrirPopup").onclick=function(){
+	document.getElementById("overlay").style.visibility="visible";
+}
+
+document.getElementById("btn-cerrar-popup").onclick=function(){
+	document.getElementById("overlay").style.visibility="hidden";
+}
+
+document.getElementById("btnAbrirPopup2").onclick=function(){
+	document.getElementById("overlay2").style.visibility="visible";
+}
+
+document.getElementById("btn-cerrar-popup2").onclick=function(){
+	document.getElementById("overlay2").style.visibility="hidden";
+}
