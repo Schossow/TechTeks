a856ed7601b067e38a12af0501a20663bd888a1b
Schossow <68172401+Schossow@users.noreply.github.com>
2020-08-01 11:04:37 -0500

    actualizacion de seccion de servicios

--- js/anexoScript.js
+++ js/anexoScript.js
@@ -1,4 +1,4 @@
-
+/*Landing Page*/
 document.getElementById("btnAbrirPopup").onclick=function(){
 	document.getElementById("overlay").style.visibility="visible";
 }
@@ -6,11 +6,30 @@ document.getElementById("btnAbrirPopup").onclick=function(){
 document.getElementById("btn-cerrar-popup").onclick=function(){
 	document.getElementById("overlay").style.visibility="hidden";
 }
-
+/*Página Web Comercial*/
 document.getElementById("btnAbrirPopup2").onclick=function(){
 	document.getElementById("overlay2").style.visibility="visible";
 }
 
 document.getElementById("btn-cerrar-popup2").onclick=function(){
 	document.getElementById("overlay2").style.visibility="hidden";
+}
+
+/*One Page*/
+document.getElementById("btnAbrirPopup3").onclick=function(){
+	document.getElementById("overlay3").style.visibility="visible";
+
+}
+
+document.getElementById("btn-cerrar-popup3").onclick=function(){
+	document.getElementById("overlay3").style.visibility="hidden";
+}
+/*Mas*/
+document.getElementById("btnAbrirPopup4").onclick=function(){
+	document.getElementById("overlay4").style.visibility="visible";
+	
+}
+
+document.getElementById("btn-cerrar-popup4").onclick=function(){
+	document.getElementById("overlay4").style.visibility="hidden";
 }
