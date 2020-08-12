f4618603f6b179c486ce575d9609e5e207c2de56
Alejo <qskillztutores@gmail.com>
2020-08-11 20:22:25 -0500

    grid nuevo para los logos

--- slick/slick-1.8.1/slick/initializer.js
+++ slick/slick-1.8.1/slick/initializer.js
@@ -6,5 +6,30 @@
         autoplay: true,
         autoplaySpeed: 2000,
       });
+      $('.center').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
     });
  
