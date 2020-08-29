4f54bdc83f74e7f0adf09fcbfa4049836b7a3c5d
Alejo <qskillztutores@gmail.com>
2020-08-28 22:40:22 -0500

    v1.0.1.28082020
    Errores corregidos: \n1. qskillz.png -> qskillz.PNG para barra de logos \n2. Opciones de servicio en formulario de contacto\n3. Saltos de linea en enviar.php

--- enviar.php
+++ enviar.php
@@ -4,22 +4,21 @@
 	  $serviceselected = $_POST['seleccion'];
       $usermessage = $_POST['mensaje'];
 
-	   $myemail = "info@techteks.com, 
+	   $myemail = "info@techteks.net, 
 					   alejandrobenavidesmontano@gmail.com,
 					   schossow.1373@gmail.com"; //Hacia donde va
 	   $ehead = "Content-Type: text/html; charset=UTF-8"; //Caracteres especiales
-       $emess = "Nombre: ".$username."\n"; //Nombre
-	   $emess.= "Email: ".$useremail."\n"; //Email
-	   $emess.= "Servicio de interés: ".$serviceselected."\n"; //Servicio de interes
-	   $emess.= "Comentarios adicionales: ".$usermessage."\n"; //Mensaje complementario
+       $emess = "Nombre: ".$username."<br>"; //Nombre
+	   $emess.= "Email: ".$useremail."<br>"; //Email
+	   $emess.= "Versión: v1.0.1.28082020"."<br>"; //Servicio de interes
+	   $emess.= "Servicio de interés: ".$serviceselected."<br>"; //Servicio de interes
+	   $emess.= "Comentarios adicionales: ".$usermessage."<br>"; //Mensaje complementario
        $subj = "Consulta de Servicios TechTeks"; //Asunto
        $mailsend = @mail("$myemail","$subj","$emess","$ehead");
       if ($mailsend){
 		 header('Location: contact.html');
-		 echo "Mensaje enviado con éxito!";
 	  }
 	  else{
-		  echo "No se pudo enviar el Email porque ocurrió un error!";
 		  header('Location: contact.html');
 	  }
       
