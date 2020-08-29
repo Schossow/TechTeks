2aef8deb18775dd903138c899c7882cfc939fc6f
Alejo <qskillztutores@gmail.com>
2020-08-28 20:43:30 -0500

    casi listo, falta la frase de quezada

--- enviar.php
+++ enviar.php
@@ -1,14 +1,26 @@
-<?php 
+<?php
+      $username = $_POST['nombre'];
+	  $useremail = $_POST['email'];
+	  $serviceselected = $_POST['seleccion'];
+      $usermessage = $_POST['mensaje'];
 
-	$destino= "info@techteks.net";
-	$nombre= $_POST["nombre"];
-	$correo= $_POST["correo"];
-	$asunto= $_POST["asunto"];
-	$mensaje= $_POST["mensaje"];
-
-	$contenido= "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nAsunto: " . $asunto . "\nMensaje: " . $mensaje;
-	
-	$verification = mail($destino, $asunto, $contenido);
-	header("Location:index.html");
-
+	   $myemail = "info@techteks.com, 
+					   alejandrobenavidesmontano@gmail.com,
+					   schossow.1373@gmail.com"; //Hacia donde va
+	   $ehead = "Content-Type: text/html; charset=UTF-8"; //Caracteres especiales
+       $emess = "Nombre: ".$username."\n"; //Nombre
+	   $emess.= "Email: ".$useremail."\n"; //Email
+	   $emess.= "Servicio de interés: ".$serviceselected; //Servicio de interes
+	   $emess.= "Comentarios adicionales: ".$usermessage; //Mensaje complementario
+       $subj = "Consulta de Servicios TechTeks"; //Asunto
+       $mailsend = @mail("$myemail","$subj","$emess","$ehead");
+      if ($mailsend){
+		 header('Location: contact.html');
+		 echo "Mensaje enviado con éxito!";
+	  }
+	  else{
+		  echo "No se pudo enviar el Email porque ocurrió un error!";
+		  header('Location: contact.html');
+	  }
+      
 ?>
